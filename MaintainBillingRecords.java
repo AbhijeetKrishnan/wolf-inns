8b7b1d563f53a3e1fe3123321bce8069bb791d50
Abhijeet Krishnan <akrish13@ncsu.edu>
2018-03-24 18:21:28 -0400

    Add CRU for BillingInfo

--- MaintainBillingRecords.java
+++ MaintainBillingRecords.java
@@ -169,5 +169,138 @@ public class MaintainBillingRecords {
 		
 		return false;
 	}
-
+	
+	/**
+	 * Creates a new billing_info record
+	 * @param String responsiblePartySSN
+	 * @param String address
+	 * @param String city
+	 * @param String state
+	 * @param String payMethodCode
+	 * @param String cardNumber
+	 * @return BillingInfo object which contains the newly inserted fields
+	 */
+	public static BillingInfo createBillingInfo(String responsiblePartySSN, String address, String city, String state, String payMethodCode, String cardNumber) {
+		
+		String sqlStatement = "INSERT INTO billing_info (responsiblePartySSN, address, city, state, payMethodCode, cardNumber) VALUES (?, ?, ?, ?, ?, ?)";
+		Connection connection = null;
+		PreparedStatement statement = null;
+		BillingInfo billingInfo = null;
+		ResultSet generatedKeys = null;
+		
+		try {
+			connection = DatabaseConnection.getConnection();
+			statement = connection.prepareStatement(sqlStatement);
+			
+			statement.setString(1, responsiblePartySSN);
+			statement.setString(2, address);
+			statement.setString(3, city);
+			statement.setString(4, state);
+			statement.setString(5, payMethodCode);
+			statement.setString(6, cardNumber);
+			
+			int rowsAffected = statement.executeUpdate();
+			
+			if (rowsAffected == 1) {
+				billingInfo = new BilingInfo();
+				generatedKeys = statement.getGeneratedKeys();
+				generatedKeys.next();
+				billingInfo.setBillingId(generatedKeys.getInt(1));
+				billingInfo.setResponsiblePartySSN(responsiblePartySSN);
+				billingInfo.setAddress(address);
+				billingInfo.setCity(city);
+				billingInfo.setState(state);
+				billingInfo.setPayMethodCode(payMethodCode);
+				billingInfo.setCardNumber(cardNumber);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try { generatedKeys.close(); } catch (Exception ex) {};
+			try { statement.close(); } catch (Exception ex) {};
+			try { connection.close(); } catch (Exception ex) {};
+		}
+		
+		return billingInfo;
+	}
+	
+	/**
+	 * Retrieve a record of billing_info table from the database
+	 * @param int billingId: the billingId of the record which is to be retrieved from the table
+	 * @return BillingInfo object which is null in case billingId does not exist in the table
+	 */
+	public static boolean retrieveBillingInfo(int billingId) {
+		
+		String sqlStatement = "SELECT * FROM billing_info WHERE billingId = ?";
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet results = null;
+		BillingInfo billingInfo = null;
+		
+		try {
+			connection = DatabaseConnection.getConnection();
+			statement = connection.prepareStatement(sqlStatement);
+			statement.setInt(1, billingId);
+			results = statement.executeQuery();
+			
+			while (results.next()) { // expect only 0/1 row
+				billingInfo = new BillingInfo();
+				billingInfo.setResponsiblePartySSN(results.getString("responsiblePartySSN"));
+				billingInfo.setAddress(results.getString("address"));
+				billingInfo.setCity(results.getString("city"));
+				billingInfo.setState(results.getString("state"));
+				billingInfo.setPayMethodCode(results.getString("payMethodCode"));
+				billingInfo.setCardNumber(results.getString("cardNumber"));
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try { results.close(); } catch (Exception ex) {};
+			try { statement.close(); } catch (Exception ex) {};
+			try { connection.close(); } catch (Exception ex) {};
+		}
+		
+		return billingInfo;
+	}
+	
+	/**
+	 * Update a record of billing_info table in the database
+	 * @param BillingInfo billingInfo: the object which is to be updated in the table
+	 * @return boolean which is true on success, false on failure
+	 */
+	public static boolean updateBillingInfo(BillingInfo billingInfo) {
+		
+		String sqlStatement = "UPDATE billing_info SET responsiblePartySSN = ?, address = ?, city = ?, state = ?, payMethodCode = ?, cardNumber = ? WHERE billingId = ?";
+		Connection connection = null;
+		PreparedStatement statement = null;
+		boolean returnValue = false;
+		
+		try {
+			connection = DatabaseConnection.getConnection();
+			statement = connection.prepareStatement(sqlStatement);
+			
+			statement.setString(1, responsiblePartySSN);
+			statement.setString(2, address);
+			statement.setString(3, city);
+			statement.setString(4, state);
+			statement.setString(5, payMethodCode);
+			statement.setString(6, cardNumber);
+			statement.setInt(7, billingInfo.getBillingId());
+			
+			int rowsAffected = statement.executeUpdate();
+			
+			if (rowsAffected == 1) {
+				returnValue = true;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try { statement.close(); } catch (Exception ex) {};
+			try { connection.close(); } catch (Exception ex) {};
+		}
+		
+		return returnValue;
+	}
+	
+	
 }
