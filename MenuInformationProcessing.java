b07ba4e42d53f4c4a921765efb06350d83a7e694
Chekad Sarami <csarami@ncsu.edu>
2018-04-14 09:14:35 -0400

    Added menuOptionUpdateStay

--- MenuInformationProcessing.java
+++ MenuInformationProcessing.java
@@ -1179,6 +1179,61 @@ public class MenuInformationProcessing {
 		}
     }
 	
+	private static void menuOptionUpdateStay() {
+        System.out.println("|---------------------------------------------------------------------|");
+        System.out.println("| UPDATE STAY RECORD                                                 |");
+        System.out.println("|---------------------------------------------------------------------|\n\n");
+        Stays h = Stays.select();
+        if (null == h) {
+            System.out.println("No record was selected.");
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            String[] field = {"hotelId", "roomNumber", "customerId", "numOfGuests", "checkinDate", "checkinTime",
+            "checkoutDate","checkoutTime","billingId"};
+            String[] current = {Integer.toString(h.getHotelId()), h.getRoomNumber(), Integer.toString(h.getCustomerId()), 
+                Integer.toString(h.getNumOfGuests()), h.getCheckinDate(),
+                h.getCheckinTime(), h.getCheckoutDate(), h.getCheckoutTime(),Integer.toString( h.getBillingId())};
+            String[] update = {Integer.toString(h.getHotelId()), h.getRoomNumber(), Integer.toString(h.getCustomerId()), 
+                Integer.toString(h.getNumOfGuests()), h.getCheckinDate(),
+                h.getCheckinTime(), h.getCheckoutDate(), h.getCheckoutTime(),Integer.toString( h.getBillingId())};
+            System.out.println("Current stay id: " + h.getStayId());
+            for (int i = 0; i < field.length; i++) {
+                System.out.println("Current hotel " + field[i] + ": " + current[i]);
+                System.out.println("Do you want to update this field? (Y/N)");
+                String response = "";
+                do {
+                    System.out.print("===> ");
+                    while (!scanner.hasNextLine()) {
+                        System.out.println("===> ");
+                    }
+                    response = scanner.nextLine();
+                } while (!response.equals("Y") && !response.equals("N"));
+                if (response.equals("Y")) {
+                    System.out.print("===> ");
+                    while (!scanner.hasNextLine()) {
+                        System.out.println("===> ");
+                    }
+                    update[i] = scanner.nextLine();
+                }
+            }
+            h.setHotelId(Integer.parseInt(update[0]));
+            h.setRoomNumber(update[1]);
+            h.setCustomerId(Integer.parseInt(update[2]));
+            h.setNumOfGuests(Integer.parseInt(update[3]));
+            h.setCheckinDate(update[4]);
+            h.setCheckinTime(update[5]);
+            h.setCheckoutDate(update[6]);
+            h.setCheckoutTime(update[7]);
+            h.setBillingId(Integer.parseInt(update[8]));
+            
+            if (InformationProcessing.updateStay(h)) {
+                System.out.println("Record updated successfully.");
+            } else {
+                System.out.println("Something unexpected happened while attempting to update the record.");
+            }
+        }
+    }
+	
 	public static void menuOptionReprintItemizedReceipt() {
 		System.out.println("|---------------------------------------------------------------------|");
 		System.out.println("| REPRINT ITEMIZED RECEIPT FOR STAY                                   |");
