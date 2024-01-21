bc73cd17dd83755d9939508bac9a03659f6df9b2
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2024-01-20 21:51:15 -0500

    Update DB credentials

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -4,10 +4,10 @@ import java.util.LinkedHashMap;
 
 public class DatabaseConnection {
 
-	private static final String user = "xzheng6";
-	private static final String password = "200016878";
+	private static final String user = "akrish13";
+	private static final String password = "";
 	
-	private static final String connectionURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/" + user;	
+	private static final String connectionURL = "jdbc:mariadb://localhost:3306/test";	
 	
 	/**
 	 * This method will create and return a new Connection object that is attached to 
